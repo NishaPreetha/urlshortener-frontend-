ce2508dd35a59563c481399a15e7873ac90e3a29
nisha <nishapreethamurugan@gmail.com>
2024-05-24 18:48:29 +0530

    Initial commit

---
+++ urlshortener-frontend-main/src/Pages/ForgotPage.jsx
@@ -0,0 +1,11 @@
+import Forgot from "../Components/Forgot";
+
+export default function ForgotPage(){
+
+    //forgotPage component
+    return(
+        <div className="forgot-page">
+            <Forgot/>
+        </div>
+    )
+}
